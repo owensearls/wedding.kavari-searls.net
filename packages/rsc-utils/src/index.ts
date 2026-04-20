8cc5a977f931cfc8513411f233297d959b545c46
Owen Searls <owen@searls.net>
2026-04-19 22:47:04 -0400

    Add rscBrowser plugin
    Browser compat shim for consumer SPAs that import the RSC client stubs
    without registering @vitejs/plugin-rsc themselves. Replaces the inline
    rscBrowserCompat function currently in packages/frontend/vite.config.ts.

--- packages/rsc-utils/src/index.ts
+++ packages/rsc-utils/src/index.ts
@@ -1,2 +1,3 @@
+export { rscBrowser } from './plugins/browser-compat'
 export { rscFunctions } from './plugins/functions'
 export type { CorsOptions, FunctionsConfig, NamespaceConfig } from './types'
